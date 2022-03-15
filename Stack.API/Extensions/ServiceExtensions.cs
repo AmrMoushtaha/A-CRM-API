5cffa6601ac932849494b1262faec393e0b8ade3
Amr Moushtaha <amrmoushtaha@gmail.com>
2022-03-15 23:30:37 +0200

    Cleaned Project

--- Stack.API/Extensions/ServiceExtensions.cs
+++ Stack.API/Extensions/ServiceExtensions.cs
@@ -1,10 +1,8 @@
 ﻿
 using Microsoft.Extensions.DependencyInjection;
 using Stack.ServiceLayer.Modules.Auth;
-using Stack.ServiceLayer.Modules.Employees;
-using Stack.ServiceLayer.Modules.Organizations;
 using Stack.Repository.Common;
-using Stack.ServiceLayer.Modules.Materials;
+
 
 namespace Stack.API.Extensions
 {
@@ -16,17 +14,6 @@ namespace Stack.API.Extensions
 
             caller.AddScoped<UsersService>();
             caller.AddScoped<RolesService>();
-            caller.AddScoped<EmployeesService>();
-            caller.AddScoped<CompanyCodesService>();
-            caller.AddScoped<PurchasingGroupsService>();
-            caller.AddScoped<OrgUnitsService>();
-            caller.AddScoped<CostCentersService>();
-            caller.AddScoped<PlantsService>();
-            caller.AddScoped<MaterialGroupsService>();
-            caller.AddScoped<UOMsService>();
-            caller.AddScoped<PositionsService>();
-            caller.AddScoped<MaterialTypesService>();
-            caller.AddScoped<MaterialService>();
 
         }
 
