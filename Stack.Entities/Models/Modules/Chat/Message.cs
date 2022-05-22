db50a5d32d2587c2ecaf9654046bbf24d812d5de
YG <71011105+YGhazy@users.noreply.github.com>
2022-05-23 00:06:44 +0200

    initialize chat

---
+++ Stack.Entities/Models/Modules/Chat/Message.cs
@@ -0,0 +1,26 @@
+﻿using Stack.DTOs.Models.Modules.CustomerStage;
+using Stack.Entities.Models.Modules.Auth;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Stack.Entities.Models.Modules.Chat
+{
+    public class Message
+    {
+
+        public long ID { get; set; }
+        public string SenderID { get; set; }
+
+        public long ConversationID { get; set; }
+
+        [ForeignKey("ConversationID")]
+        public virtual Conversation Conversation { get; set; }
+
+        public string Content { get; set; }
+        public bool Seen { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+}
