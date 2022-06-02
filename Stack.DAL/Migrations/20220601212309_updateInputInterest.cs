6af9fce46c523147220f68c2abf29e37b072dc71
YG <71011105+YGhazy@users.noreply.github.com>
2022-06-02 02:05:55 +0200

    update interest inputs

---
+++ Stack.DAL/Migrations/20220601212309_updateInputInterest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stack.DAL.Migrations
+{
+    public partial class updateInputInterest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AttributeID",
+                table: "LInterestInputs",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "InputType",
+                table: "LInterestInputs",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PredefinedInputType",
+                table: "LInterestInputs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttributeID",
+                table: "LInterestInputs");
+
+            migrationBuilder.DropColumn(
+                name: "InputType",
+                table: "LInterestInputs");
+
+            migrationBuilder.DropColumn(
+                name: "PredefinedInputType",
+                table: "LInterestInputs");
+        }
+    }
+}
